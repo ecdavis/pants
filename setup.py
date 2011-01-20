cc0185f91b570ca30c80f52201ee7fa4147d355f
Chris Davis <davis@wtfrak.com>
2011-01-20 15:11:19 +1100

    Added a setup script.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="pants",
+    version="0.9.0",
+    description="A lightweight framework for writing asynchronous network applications in Python.",
+    author="Chris Davis",
+    author_email="chris@wtfrak.com",
+    url="http://pants.wtfrak.com/",
+    download_url="http://pants.wtfrak.com/release/pants-0.9.0.zip",
+    packages=["pants"],
+    classifiers=[
+        "Programming Language :: Python :: 2.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    )
