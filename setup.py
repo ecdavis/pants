eb030fe16be16a75a513a681d706b3fae53b7add
Chris Davis <davis@wtfrak.com>
2011-01-23 12:27:28 +1100

    Updated setup.py for 0.9.2

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name="pants",
-    version="0.9.1",
+    version="0.9.2",
     description="A lightweight framework for writing asynchronous network applications in Python.",
-    author="Chris Davis",
+    author="Christopher Davis",
     author_email="chris@wtfrak.com",
     url="http://pants.wtfrak.com/",
-    download_url="http://pants.wtfrak.com/release/pants-0.9.1.zip",
+    download_url="http://pants.wtfrak.com/release/pants-0.9.2.zip",
     packages=["pants"],
     classifiers=[
         "Programming Language :: Python :: 2.6",
