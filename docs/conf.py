06b8a3d7827759843ce94a387b2390b9231dc3b1
Chris Davis <davis@wtfrak.com>
2011-07-24 16:48:19 +1000

    Set docs theme to default when READTHEDOCS environment variable is True.

--- docs/conf.py
+++ docs/conf.py
@@ -101,7 +101,10 @@ intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+if os.environ.get('READTHEDOCS', None) == 'True':
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
