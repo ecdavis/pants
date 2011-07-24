299cc691234c21855263df69efb0f2469dece471
Chris Davis <davis@wtfrak.com>
2011-07-24 17:15:07 +1000

    Set the docs theme to default, rather than choosing between themes.

--- docs/conf.py
+++ docs/conf.py
@@ -101,10 +101,7 @@ intersphinx_mapping = {'python': ('http://docs.python.org/', None)}
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if os.environ.get('READTHEDOCS', None) == 'True':
-    html_theme = 'default'
-else:
-    html_theme = 'nature'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
