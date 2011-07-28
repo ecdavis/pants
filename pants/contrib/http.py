5bb0cfd81b2da884293a9c12d4f5cf698f5fee49
Chris Davis <davis@wtfrak.com>
2011-07-28 18:46:56 +1000

    Added the word 'Developers' to the copyright header.

--- pants/contrib/http.py
+++ pants/contrib/http.py
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# Copyright 2011 Pants (see AUTHORS.txt)
+# Copyright 2011 Pants Developers (see AUTHORS.txt)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
