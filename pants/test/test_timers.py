d6a644546dffb46de8e27c7af0107b2edab2c712
Chris Davis <davis@wtfrak.com>
2012-03-01 09:56:48 +1100

    Added unit tests for timer cancellation.

--- pants/test/test_timers.py
+++ pants/test/test_timers.py
@@ -17,12 +17,29 @@ class TestTimers(unittest.TestCase):
         pants.engine.poll(0.01)
         self.assertEquals(len(self.times_called), 1)
 
+    def test_callback_cancel(self):
+        cancel_callback = pants.callback(self.timer)
+        cancel_callback()
+        pants.engine.poll(0.01)
+        pants.engine.poll(0.01)
+        pants.engine.poll(0.01)
+        self.assertEquals(len(self.times_called), 0)
+
     def test_loop(self):
         pants.loop(self.timer)
         pants.engine.poll(0.01)
         pants.engine.poll(0.01)
         pants.engine.poll(0.01)
         self.assertEquals(len(self.times_called), 3)
+
+    def test_loop_cancel(self):
+        cancel_loop = pants.loop(self.timer)
+        pants.engine.poll(0.01)
+        pants.engine.poll(0.01)
+        self.assertEquals(len(self.times_called), 2)
+        cancel_loop()
+        pants.engine.poll(0.01)
+        self.assertEquals(len(self.times_called), 2)
 
     def test_defer(self):
         expected_time = time.time() + 0.01
@@ -32,6 +49,14 @@ class TestTimers(unittest.TestCase):
         pants.engine.poll(0.2)
         self.assertEquals(len(self.times_called), 1)
         self.assertLess(abs(expected_time - self.times_called[0]), 0.01)
+
+    def test_defer_cancel(self):
+        cancel_defer = pants.defer(0.01, self.timer)
+        cancel_defer()
+        pants.engine.poll(0.2)
+        pants.engine.poll(0.2)
+        pants.engine.poll(0.2)
+        self.assertEquals(len(self.times_called), 0)
 
     def test_defer_with_zero_delay(self):
         self.assertRaises(ValueError, pants.defer, 0, self.timer)
@@ -49,6 +74,18 @@ class TestTimers(unittest.TestCase):
         self.assertLess(abs(expected_times[0] - self.times_called[0]), 0.01)
         self.assertLess(abs(expected_times[1] - self.times_called[1]), 0.01)
         self.assertLess(abs(expected_times[2] - self.times_called[2]), 0.01)
+
+    def test_cycle_cancel(self):
+        expected_times = [time.time() + 0.01, time.time() + 0.02, time.time() + 0.03]
+        cancel_cycle = pants.cycle(0.01, self.timer)
+        pants.engine.poll(0.2)
+        pants.engine.poll(0.2)
+        self.assertEquals(len(self.times_called), 2)
+        cancel_cycle()
+        pants.engine.poll(0.2)
+        self.assertEquals(len(self.times_called), 2)
+        self.assertLess(abs(expected_times[0] - self.times_called[0]), 0.01)
+        self.assertLess(abs(expected_times[1] - self.times_called[1]), 0.01)
 
     def test_cycle_with_zero_delay(self):
         self.assertRaises(ValueError, pants.cycle, 0, self.timer)
