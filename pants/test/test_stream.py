cfb7cda0b0bd81aace18b7181b5f94c07c610091
Evan Davis <me@ezdwt.com>
2013-09-28 17:20:45 +1000

    Added a sock.close call to test_stream

--- pants/test/test_stream.py
+++ pants/test/test_stream.py
@@ -10,6 +10,7 @@ class TestStream(unittest.TestCase):
     def test_stream_constructor_with_invalid_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.assertRaises(TypeError, Stream, socket=sock)
+        sock.close()
 
     def test_stream_set_read_delimiter_invalid_value(self):
         # not a great test, given that the error needs to be raised
